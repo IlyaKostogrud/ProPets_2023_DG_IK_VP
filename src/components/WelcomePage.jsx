bb1be6df1e0fe5dc8eabc122968abdf20e32320a
Daniel Emdin(Gavrilov) <nikeepoid@yandex.ru>
2023-03-20 21:31:47 +0200

    Added jumps on all components, new reducer in Redux, login and registration now are one component, buttons don't create any objects, only jumps between

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,33 +1,40 @@
 import React, {useState} from 'react';
 import Header from "./Header";
 import LoginRegistrationStep from "./LoginRegistrationStep";
+import {useDispatch} from "react-redux";
+import {renderLogin} from "../store/renderLoginSlice";
+import {HOME, LOST_FORM} from "../utils/constants";
 
-let logOrReg;
+let whatToRenderNext;
 const WelcomePage = () => {
     const [condition, setCondition] = useState(false);
+    const dispatch = useDispatch();
 
-    const handleClickProceed = (bool) =>{
-        logOrReg = bool;
-        setCondition(true);
+    const handleClickProceed = (bool, display = HOME) => {
+        whatToRenderNext = display;
+        dispatch(renderLogin(bool));
+        changeCondition();
+    };
+
+    const changeCondition = () => {
+        setCondition(!condition);
     };
 
     return (
         <div className={'welcome-page container'}>
             <div className={'row'}>
                 <div className={'col-12'}>
-                    <Header h_color={'green'}/>
+                    <Header h_color={'green'} handleClickProceed={handleClickProceed}/>
                 </div>
             </div>
             <div className={'row'}>
                 <div className={'col-1'}>
-                    <button onClick={() => handleClickProceed(true)}>I lost my pet!</button>
-                    <button onClick={() => handleClickProceed(true)}>I found a pet!</button>
-                    <button onClick={() => handleClickProceed(false)}>JOIN</button>
+                    <button onClick={()=>handleClickProceed(false, LOST_FORM)}>I lost my pet!</button>
+                    <button onClick={()=>handleClickProceed(false)}>I found a pet!</button>
+                    <button onClick={()=>handleClickProceed(false)}>JOIN</button>
                 </div>
             </div>
-
-
-            {condition && <LoginRegistrationStep logOrReg={logOrReg}/>}
+            {condition && <LoginRegistrationStep changeCondition={changeCondition} whatToRenderNext={whatToRenderNext}/>}
         </div>
     );
 };
