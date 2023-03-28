09c6cfc83adab363ca8cb87ed9f0212ae84bf66b
IlyaKostogrud <ikostogrud@gmail.com>
2023-03-28 16:04:58 +0300

    Post Home added
    Post Home added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PostItemHome from "./PostItemHome";
+import {lorem_ipsum} from "../utils/constants";
+import PostListHome from "./PostListHome";
 
 const Home = () => {
+    const [posts, setPosts] = useState([
+        {postID:'1111',authorName:'John Goodboi',authorIcon:'author Icon',postTime:Date.now(),picture:'Picture',text:lorem_ipsum},
+        {postID:'2222',authorName:'John Goodboi',authorIcon:'author Icon',postTime:Date.now(),picture:'Picture',text:lorem_ipsum},
+        {postID:'3333',authorName:'John Goodboi',authorIcon:'author Icon',postTime:Date.now(),picture:'Picture',text:lorem_ipsum}
+    ])
     return (
         <div>
-            Home
+            <PostListHome posts={posts} title={'Home Page'}/>
         </div>
     );
 };
