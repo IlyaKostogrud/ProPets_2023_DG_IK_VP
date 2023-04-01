12ceed1afb1b91f2298a215f3b338893fef9387b
pairodox <povol.vad@gmail.com>
2023-04-01 20:45:57 +0300

    added error page, removed wrong links

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <h2>
+            O-o-o-ops! 404 page not found!
+        </h2>
+    );
+};
+
+export default ErrorPage;
